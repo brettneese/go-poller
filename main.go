5bc4f9e9b8c7deac248260079afbbc6dbffe622f
Brett Neese <brett@neese.rocks>
2018-03-11 14:30:19 -0700

    add md5'ing/writing file

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -10,8 +12,6 @@ import (
 	jmespath "github.com/jmespath/go-jmespath"
 	viper "github.com/spf13/viper"
 )
-
-type JSONBlob []byte
 
 // https://groups.google.com/forum/#!topic/golang-nuts/W1KJQr35NE0
 func doEvery(d time.Duration, f func(time.Time)) {
@@ -24,9 +24,11 @@ func getData(t time.Time) {
 	var response interface{}
 
 	url := viper.GetString("PROVIDER_URL")
+	jmespathExpression := viper.GetString("JMESPATH_EXPRESSION")
 
+	// https://blog.alexellis.io/golang-json-api-client/
 	httpClient := http.Client{
-		Timeout: time.Second * 10, // Maximum of 2 secs
+		Timeout: time.Second * 5, // Maximum of 5 secs
 	}
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
@@ -49,20 +51,39 @@ func getData(t time.Time) {
 		log.Fatal(jsonErr)
 	}
 
-	processData(response)
+	// filter out just the needed fields by supplying a jmespath expression
+	// https://github.com/jmespath/go-jmespath/issues/22#issuecomment-277719772
+	v, jmesErr := jmespath.Search(jmespathExpression, response)
+
+	if jmesErr != nil {
+		log.Fatal(jmesErr)
+	}
+
+	saveData(v)
 }
 
-func processData(jsonBlob interface{}) {
+func saveData(jsonData interface{}) {
 
-	v, err := jmespath.Search("ctatt.[route]", jsonBlob)
+	var filename string
+
+	fileJSON, _ := json.Marshal(jsonData)
+	fileMd5 := md5.Sum(fileJSON)
+
+	filename = hex.EncodeToString(fileMd5[:])
+
+	err := ioutil.WriteFile(filename, fileJSON, 0644)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(v)
+
 }
+
 func main() {
 	viper.AutomaticEnv()
+
+	viper.SetDefault("REFRESH_INTERVAL", 5000)
+	viper.SetDefault("JMESPATH_EXPRESSION", "*")
 
 	doEvery(viper.GetDuration("REFRESH_INTERVAL")*time.Millisecond, getData)
 }
